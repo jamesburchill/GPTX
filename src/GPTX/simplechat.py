250939e9544989b3c78ab117f2b547d98c35228a
James Burchill <james@jamesburchill.com>
2023-03-04 16:20:12 -0500

    .

--- src/GPTX/simplechat.py
+++ src/GPTX/simplechat.py
@@ -1,41 +1,14 @@
 import os
 import openai
-import pandas
 
 from dotenv import load_dotenv
 
 load_dotenv()   # Load all the ENV variables into your os enviroment.
 openai.api_key = os.getenv("OPENAI_API_KEY")  # Get your API key from an environment variable
 
-
-# --------------------------------------------------------------------------------
-
-
-MAXLENGTH = 80
 conversation = []
 cost_per_token = (0.002 / 1000)
 total_tokens = 0
-
-
-# --------------------------------------------------------------------------------
-
-
-def print_in_chunks(text):
-    words = text.split()
-    lines = []
-    current_line = ''
-    for word in words:
-        if len(current_line) + len(word) > MAXLENGTH:
-            lines.append(current_line.strip())
-            current_line = ''
-        current_line += word + ' '
-    lines.append(current_line.strip())
-    for line in lines:
-        print(line)
-
-
-# --------------------------------------------------------------------------------
-
 
 system_msg = input("What type of AI would you like to create?\n")
 
