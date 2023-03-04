10be1a5bcd6701158200404503469b4d07fb8242
James Burchill <james@jamesburchill.com>
2023-03-04 17:58:09 -0500

    Created function

--- src/GPTX/simplechat.py
+++ src/GPTX/simplechat.py
@@ -5,29 +5,36 @@ from dotenv import load_dotenv
 
 from common import cost_per_token
 
-load_dotenv()   # Load all the ENV variables into your os enviroment.
+load_dotenv()  # Load all the ENV variables into your os enviroment.
 openai.api_key = os.getenv("OPENAI_API_KEY")  # Get your API key from an environment variable
 
-conversation = []
-total_tokens = 0
 
-system_msg = input("What type of AI would you like to create?\n")
+def chatbot():
 
-conversation.append ({"role": "system", "content": system_msg})
+    conversation = []
+    total_tokens = 0
 
-print("Ready. Type quit() when done.")
+    system_msg = input("What type of AI would you like to create?\n")
 
-while True:
-    msg = input("YOU: ")
-    if "quit()" in msg:
-        break
-    conversation.append ({"role": "user", "content": msg})
+    conversation.append({"role": "system", "content": system_msg})
 
-    response = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=conversation)
-    reply = response["choices"][0]["message"]["content"]
-    total_tokens += response['usage']['total_tokens']
+    print("Ready. Type quit() when done.")
 
-    conversation.append({"role": "assistant", "content": reply})
-    print(reply + "\n")
+    while True:
+        msg = input("YOU: ")
+        if "quit()" in msg:
+            break
+        conversation.append({"role": "user", "content": msg})
 
-print("\nThis conversation used {} tokens and cost ${:.5f}".format(total_tokens, cost_per_token * total_tokens))
+        response = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=conversation)
+        reply = response["choices"][0]["message"]["content"]
+        total_tokens += response['usage']['total_tokens']
+
+        conversation.append({"role": "assistant", "content": reply})
+        print(reply + "\n")
+
+    print("\nThis conversation used {} tokens and cost ${:.5f}".format(total_tokens, cost_per_token * total_tokens))
+
+
+if __name__ == '__main__':
+    chatbot()
