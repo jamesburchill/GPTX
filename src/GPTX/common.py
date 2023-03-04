d6fe9e8770c97b42d97fdcc6472255661cc2cef2
James Burchill <james@jamesburchill.com>
2023-03-04 17:52:41 -0500

    .

--- src/GPTX/common.py
+++ src/GPTX/common.py
@@ -1,3 +1,21 @@
-# Common
+# Common GPTX Code
+
+# VARIABLES
 
 cost_per_token = (0.002 / 1000)
+
+
+# FUNCTIONS
+
+def print_in_chunks(text):
+    words = text.split()
+    lines = []
+    current_line = ''
+    for word in words:
+        if len(current_line) + len(word) > MAXLENGTH:
+            lines.append(current_line.strip())
+            current_line = ''
+        current_line += word + ' '
+    lines.append(current_line.strip())
+    for line in lines:
+        print(line)
